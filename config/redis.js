12832c256e399613a7220ab4dbdd98a5ccec7a06
AkashAsane <akashasane4455@gmail.com>
2024-10-09 15:05:31 +0530

    Test commit

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,8 @@
 const { createClient } = require('redis');
 
-const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST || 'redis',  
-    port: process.env.REDIS_PORT || 6379,
-  });
+const redisClient = createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+});
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
